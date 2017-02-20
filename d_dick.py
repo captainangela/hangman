8a19921d843ac683df879632864d7879feacc125
angela lui <angelalui@angelas-MacBook-Air.local>
2017-02-19 20:59:27 -0800

    new new files brotha

--- d_dick.py
+++ d_dick.py
@@ -1,5 +1,5 @@
 # 1. create a list of words
-# 2. have a welcome screen for the player with the stand (? I forget the word for it) drawn out 
+# 2. have a welcome screen for the player with the stand (? I forget the word for it) drawn out
 # 3. have the computer randomly pick a word
 # 4. count the characters in the word and print the corresponding number of on the screen _ _ _ _
 # 5. ask the player to guess a letter
@@ -9,14 +9,8 @@
 # 9. if the user does not guess the word in the amount of body parts, show's a dead man
 # 10. asks the user if they want to play again
 
-#things to add: 
-  # user has 7 guesses
-  # categories of words
-  # guess the word in the middle of letter guesses
-  # ask the user if they want to play again
-  # prevent them from typing multiple letters at a time
+ # - categories of words
 import random
-import sys
 
 HANGMANPICS = ['''
   +---+
@@ -76,67 +70,47 @@ HANGMANPICS = ['''
       |
 =========''']
 
+
+def update_display(rando_word, display, letter):
+    for idx, val in enumerate(rando_word):
+        if letter == val:
+            display[idx] = letter
+
+    return display
+
+# user has 7 tries to guess the word
+
+print(HANGMANPICS[0] + "\nWelcome to Hangwoman!\n")
+
 with open('word_bank.txt') as word_file:
   hangman_words = word_file.readlines()
 
 secure_random = random.SystemRandom()
 rando_word = secure_random.choice(hangman_words).strip()
 rando_length = len(rando_word)
+wrong_guesses = []
+num_wrong_guesses = 0
 
-def update_display (random_word, display, letter):
-  for idx, val in enumerate(rando_word):
-    if letter == val:
-      display[idx] = letter
+print "_ " * rando_length + '\n'
 
-  return display
-
-def opening_display():
-  print HANGMANPICS[0] + "\n\033[5;32;40m %s \033[m\n" % "Welcome to Hangwoman!"
-  print "_ " * rando_length + '\n'
-
-def play_hangman():
-  num_wrong_guesses = 0
-  wrong_guesses = []
-  right_guesses = []
-  rando_word_list = list(rando_word)
-  word_display = list("_"*rando_length)
-  while num_wrong_guesses < 7:
+rando_word_list = list(rando_word)
+word_display = list("_"*rando_length)
+# print _ to represent rando_word_list
+# print rando_word_list
+while num_wrong_guesses < 8:
     letter_guess = raw_input("What letter would you like to guess? ").lower()
     if letter_guess in rando_word_list:
-      right_guesses.append(letter_guess)
-      print HANGMANPICS[num_wrong_guesses] + '\n'
+      print "Yep! That letter is in there!"
       word_display = update_display(rando_word, word_display, letter_guess)
       print word_display #how to print without the quotations and commas
-      print "\nYep! That letter is in there!\n"
-    elif letter_guess == 'exit':
-      sys.exit()  
-    elif letter_guess == rando_word:
-      print "Wowza! You figured it out!"
-      sys.exit()
     else:
       if letter_guess in wrong_guesses:
         print "You've already guessed that!"
+        print word_display
       else:
         wrong_guesses.append(letter_guess)
-        #print wrong_guesses
+        print wrong_guesses
         num_wrong_guesses = len(wrong_guesses)
-        #print num_wrong_guesses
+        print num_wrong_guesses
         print HANGMANPICS[num_wrong_guesses] + "\n"
-        print word_display  
-        print "\n\nSorry, guess again."
-  print "Oh shucks, you've died!"
-
-def new_game():
-  choice = raw_input("Would you like to play again? Y or N: ").lower()
-  if choice == "y":
-    print HANGMANPICS[0]+ "\n\033[5;32;40m %s \033[m\n" % "Let's play again!"
-    print "_ " * rando_length + '\n'
-    play_hangman()
-  else:
-    sys.exit()
-
-opening_display()
-play_hangman()
-new_game()
-  
-
+        print word_display
